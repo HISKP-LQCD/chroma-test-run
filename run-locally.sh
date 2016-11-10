0a669325619252b99b9db13a4292ee071e275a5b
Martin Ueding <dev@martin-ueding.de>
2016-11-10 14:06:27 +0100

    Use local hmc if available

--- run-locally.sh
+++ run-locally.sh
@@ -6,7 +6,15 @@ set -e
 set -u
 set -x
 
-hmc -i testrun.ini.xml -o testrun.out.xml -geom 1 1 1 2 #> testrun.log.txt
+if [[ ../chroma/mainprogs/main/hmc ]]
+then
+    hmc=../chroma/mainprogs/main/hmc
+else
+    hmc=hmc
+fi
+
+
+$hmc -i testrun.ini.xml -o testrun.out.xml -geom 1 1 1 2 #> testrun.log.txt
 
 ../hmc-analysis-tools/plaquette.py testrun.out.xml
 convert testrun.out-deltaH.pdf -resize 400x400 plots/${1}-deltaH.png
