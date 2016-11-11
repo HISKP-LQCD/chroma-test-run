aab8b3ff845a9fe0359abb88e38f22c9a95d48ea
Martin Ueding <dev@martin-ueding.de>
2016-11-11 16:24:49 +0100

    Enable everything

--- run-locally.sh
+++ run-locally.sh
@@ -6,7 +6,11 @@ set -e
 set -u
 set -x
 
-OMP_NUM_THREADS=$(nproc) time hmc -i testrun.ini.xml -o testrun.out.xml > testrun.log.txt
+shopt -s nullglob 
+rm -f XMLDAT XMLLOG *.lime *_restart_*.xml
+
+#OMP_NUM_THREADS=$(nproc)
+time hmc -i testrun.ini.xml -o testrun.out.xml #> testrun.log.txt
 
 ../hmc-analysis-tools/plaquette.py testrun.out.xml
 convert testrun.out-deltaH.pdf -resize 400x400 plots/${1}-deltaH.png
