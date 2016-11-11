710396b3ae96c0a5e7ba12971c6ccbab06aa0bbe
Martin Ueding <dev@martin-ueding.de>
2016-11-11 16:16:35 +0100

    Use global chroma again

--- run-locally.sh
+++ run-locally.sh
@@ -6,15 +6,7 @@ set -e
 set -u
 set -x
 
-if [[ ../chroma/mainprogs/main/hmc ]]
-then
-    hmc=../chroma/mainprogs/main/hmc
-else
-    hmc=hmc
-fi
-
-
-OMP_NUM_THREADS=$(nproc) $hmc -i testrun.ini.xml -o testrun.out.xml -geom 1 1 1 2 > testrun.log.txt
+OMP_NUM_THREADS=$(nproc) time hmc -i testrun.ini.xml -o testrun.out.xml > testrun.log.txt
 
 ../hmc-analysis-tools/plaquette.py testrun.out.xml
 convert testrun.out-deltaH.pdf -resize 400x400 plots/${1}-deltaH.png
