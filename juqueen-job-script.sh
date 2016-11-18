ef7448162553fa89e846181703a6166f72305f7b
Martin Ueding <dev@martin-ueding.de>
2016-11-18 16:00:08 +0100

    Update job script template

--- juqueen-job-script.sh
+++ juqueen-job-script.sh
@@ -1,12 +1,13 @@
-#@ job_name = chroma-test-run
-#@ output = $(job_name)-$(jobid)-out.txt
-#@ error = $(job_name)-$(jobid)-err.txt
+#@ job_name = hmc-$(jobid)
+#@ output = $(job_name)-out.txt
+#@ error = $(job_name)-err.txt
 #@ environment = COPY_ALL
 #@ wall_clock_limit = 00:30:00
-#@ notification = error
+#@ notification = always
 #@ notify_user = ueding@hiskp.uni-bonn.de
 #@ job_type = bluegene
 #@ bg_size = 32
 #@ queue
 
-LD_LIBRARY_PATH=local/lib runjob --ranks-per-node 1 : ../local/bin/hmc -i testrun.ini.xml -o testrun.out.xml
+runjob --ranks-per-node 1 : \
+    hmc -i testrun.ini.xml -o testrun.out.xml
