5b80f736efe782292e010757348bdfcf8c63bf96
Martin Ueding <dev@martin-ueding.de>
2016-11-29 14:44:43 +0100

    Change shebang

--- jureca-submit-with-modules.sh
+++ jureca-submit-with-modules.sh
@@ -1,8 +1,12 @@
-#!/bin/bash -e -u -x
+#!/bin/bash
 # Copyright © 2016 Martin Ueding <dev@martin-ueding.de>
 
 # Submits the job to the JURECA queue and makes sure that the needed modules to
 # run `hmc` are loaded before submitting.
+
+set -e
+set -u
+set -x
 
 module load Intel
 module load IntelMPI
