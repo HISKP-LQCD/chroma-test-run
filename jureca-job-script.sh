27da95a1c3c820ce202d3ae15dc914f29f358c01
Martin Ueding <dev@martin-ueding.de>
2016-12-06 10:10:36 +0100

    Add QPhiX options

--- jureca-job-script.sh
+++ jureca-job-script.sh
@@ -3,6 +3,10 @@
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
 #SBATCH --time=00:30:00
-#BATCH --partition=batch
+#SBATCH --partition=batch
+#SBATCH --cpus-per-task=24
+#SBATCH --mail-type=ALL
 
-srun ./hmc -i testrun.ini.xml -o testrun.out.xml
+export OMP_NUM_THREADS=${SLURM_CPUS_PER_TASK}
+srun ./hmc -i testrun.ini.xml -o testrun.out.xml \
+    -by 8 -bz 8 -c 16 -sy 1 -sz 2 -pxy 0 -pxyz 0 -minct 2
